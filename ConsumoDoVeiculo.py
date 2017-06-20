18ea94006ff1d2e19237aef774e2f3821ce7d9a7
Alewarrior <rocha.alessandro@gmail.com>
2017-06-20 09:36:41 -0300

    Update ConsumoDoVeiculo.py

--- ConsumoDoVeiculo.py
+++ ConsumoDoVeiculo.py
@@ -1,6 +1,6 @@
 kmPercorridos = int(input("Quantos Km foram percorridos ?"))
-Lgastos = int(input("Quantos litros foram gastos ?"))
-consumo = kmPercorridos/Lgastos
+ltsconsumidos = int(input("Quantos litros foram gastos ?"))
+consumo = kmPercorridos/ltsconsumidos
 print("O consumo é de %d km/l"%(consumo))
 
 
